5ced91ed85f8a7e13d6559f9812a072e90bc9a80
Rishin Madan <rishin.madan@gmail.com>
2022-11-12 20:01:58 +0000

    Set up library lenspy

---
+++ lenspy/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='lens.py',
+    packages=find_packages(include=['lens.py']),
+    version='0.1.0',
+    description='',
+    author='Me',
+    license'MIT',
+)
