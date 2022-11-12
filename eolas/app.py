3b9ddd12bd69153b9e7ea48bb751ed51b8c2d9b5
Toby <toby@tobias.codes>
2022-11-12 20:16:43 +0000

    flask setup done

--- eolas/app.py
+++ eolas/app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-	return "<p>Hello, World!</p>"
+from flask import render_template
+
+@app.route('/')
+def home():
+	return render_template(
+		'home.html',
+		title="Eolas",
+		description="Social research network"
+	)
